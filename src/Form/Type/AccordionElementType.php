074c2ad0fd4994e1810d2c657f296cd082837bbe
Maxime Huran <m.huran@monsieurbiz.com>
2024-09-05 12:33:49 +0200

    Add accordion element

---
+++ src/Form/Type/AccordionElementType.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz's SyliusUiElementsPlugin for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusUiElementsPlugin\Form\Type;
+
+use MonsieurBiz\SyliusRichEditorPlugin\Form\Type\WysiwygType;
+use MonsieurBiz\SyliusRichEditorPlugin\WysiwygEditor\EditorInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AccordionElementType extends AbstractType
+{
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_ui_elements.common.fields.title',
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+            ->add('description', WysiwygType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_ui_elements.common.fields.description',
+                'editor_toolbar_type' => EditorInterface::TOOLBAR_TYPE_CUSTOM,
+                'editor_toolbar_buttons' => [
+                    ['undo', 'redo'],
+                    ['bold', 'underline', 'italic', 'strike'],
+                    ['fontColor', 'hiliteColor'],
+                    ['removeFormat'],
+                    ['link'],
+                    ['showBlocks', 'codeView'],
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+        ;
+    }
+}
